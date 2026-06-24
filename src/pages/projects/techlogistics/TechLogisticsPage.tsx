64e5a0f356912e04fd0c18b5c6d0e363b7f4bbc8
Baruc Alejandria <alejandriacbaruc@gmail.com>
2026-06-24 02:24:18 -0500

    feat(techlogistics): add Objectives section to TechLogistics Page

--- src/pages/projects/techlogistics/TechLogisticsPage.tsx
+++ src/pages/projects/techlogistics/TechLogisticsPage.tsx
@@ -1,10 +1,12 @@
 import { HeroTechLogistics } from './components/HeroTechLogistics';
+import { ObjectivesTechLogistics } from './components/ObjectivesTechLogistics';
 import { ArchitectureTechLogistics } from './components/ArchitectureTechLogistics';
 
 const TechLogisticsPage = () => {
   return (
     <>
       <HeroTechLogistics id="hero" />
+      <ObjectivesTechLogistics id="challenge" />
       <ArchitectureTechLogistics id="tabs" />
     </>
   );
